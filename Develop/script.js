ca4ae5ad142af3301326d3dcb8b136c9e75f7946
rob <collinsduo1@gmail.com>
2023-05-15 16:13:49 -0500

    add funtion password, add eventlistener

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,24 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+/* Password variable below */
+/* button info and assignment code */
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
+//* input for passwords */
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
+
 // Add event listener to generate button
+// //  generate btn
 generateBtn.addEventListener("click", writePassword);
+
+// Array of  characters */
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialChar = ["!", "@", "#", "$", "%", "*", "=", "?", ">", "<"];

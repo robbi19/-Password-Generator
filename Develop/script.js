286ccca27d331e370d27add5c1ced597ca700cd3
rob <collinsduo1@gmail.com>
2023-05-21 22:12:10 -0500

    fix array and if/else statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,115 +1,115 @@
-/* Password variable below */
-/* button info and assignment code */
 var generateBtn = document.querySelector("#generate");
 
 //* User input */
 function getPasswordOptions(userNumCharacters) {
   if (isNaN(userNumCharacters)) {
-    alert("Did you forget to put in a Number?");
+    alert("Did you forget a number?");
     return false;
   } else if (parseInt(userNumCharacters) < 8) {
-    alert("Reminder:password has to be at least 8  characters");
+    alert("password has to be 8 characters");
     return false;
   } else if (parseInt(userNumCharacters) >= 128) {
-    alert("Reminder:password has to be less than 128 characters");
+    alert("pw has to be less than 128 characters");
     return false;
   }
   return true;
 }
 
-// random characters  */
+// random char  */
 function getRandomElementFromArray(collection) {
   return collection[Math.floor(Math.random() * collection.length)];
 }
 
-// Write password to the #password input */
+// #password input */
 
-/* if and else section, user password area*/
-function generatePassword() 
+/* if and else section*/
+function generatePassword() {
   var userNumCharacters = prompt(
     "How many characters would you like your password to be? Pick a number between 8-128."
   );
-  /* Confirm  and validation section */
+  /* Confirm area */
   var passwordValid = getPasswordOptions(userNumCharacters);
   if (passwordValid) {
     var hasSpecialChar = confirm(
-      "This will include special characters in your password?"
+      "This will include special characters in password?"
     );
-    var hasNumbers = confirm("This will include numbers in your password?");
+    var hasNumbers = confirm("This will include numbers in password?");
 
     var hasLowerCase = confirm(
-      "This will include lowercase characters in your password?"
+      "This will include lowercase characters in password?"
     );
     var hasUpperCase = confirm(
-      "This will include uppercase characters in your password?"
+      "This will include uppercase characters in password?"
     );
   }
- //Sentence to check everything */
- if (
-  [hasSpecialChar, hasNumbers, hasLowerCase, hasUpperCase].includes(
-    true
+  //Sentence to check  */
+  if (
+    [hasSpecialChar, hasNumbers, hasLowerCase, hasUpperCase].includes(
+      true
+    )
   )
-)
-  //Array to store characters into  password */
-  var chosenChar = [];
+    //Array to store char into pw */
+    var chosenChar = [];
 
-// each type of  character to ensure each will be used */
-var guaranteedChar = [];
+  //  type of char to ensure used */
+  var guaranteedChar = [];
 
-// add array of each type of all character  */
-if (hasSpecialChar) {
-  chosenChar = chosenChar.concat(specialChar);
-  guaranteedChar.push(
-    specialChar[Math.floor(Math.random() * specialChar.length)]
-  );
-}
-if (hasNumbers) {
-  chosenChar = chosenChar.concat(numbers);
-  guaranteedChar.push(
-    numbers[Math.floor(Math.random() * numbers.length)]
-  );
-}
-if (hasLowerCase) {
-  chosenChar = chosenChar.concat(lowerCase);
-  guaranteedChar.push(
-    lowerCase[
-      Math.floor(Math.random() * lowerCase.length)
-    ]
-  );
-}
-if (hasUpperCase) {
-  chosenChar = chosenChar.concat(upperCase);
-  guaranteedChar.push(
-    upperCase[
-      Math.floor(Math.random() * upperCase.length)
-    ]
-  );
-}
-
-//*  password loop */
-var randomChar = [];
-for (var i = 0; i < userNumCharacters; i++) {
-  var index = Math.floor(Math.random() * chosenChar.length);
-  randomChar.push(chosenChar[index]);
-}
- //Loop to another part */.
-var replacedPosition = {};
-while (guaranteedChar.length > 0) {
-  var replaceChar = Math.floor(Math.random() * randomChar.length);
-  if (!replacedPosition[replaceChar]) {
-    randomChar[replaceChar] = guaranteedChar.pop();
-    replacedPosition[replaceChar] = true;
+  // add array to all char  */
+  if (hasSpecialChar) {
+    chosenChar = chosenChar.concat(specialChar);
+    guaranteedChar.push(
+      specialChar[Math.floor(Math.random() * specialChar.length)]
+    );
   }
-}
-return randomChar.join("");
+  if (hasNumbers) {
+    chosenChar = chosenChar.concat(numbers);
+    guaranteedChar.push(
+      numbers[Math.floor(Math.random() * numbers.length)]
+    );
+  }
+  if (hasLowerCase) {
+    chosenChar = chosenChar.concat(lowerCase);
+    guaranteedChar.push(
+      lowerCase[
+        Math.floor(Math.random() * lowerCase.length)
+      ]
+    );
+  }
+  if (hasUpperCase) {
+    chosenChar = chosenChar.concat(upperCase);
+    guaranteedChar.push(
+      upperCase[
+        Math.floor(Math.random() * upperCase.length)
+      ]
+    );
+  }
 
-// //* input for passwords */
+  //*  pw loop */
+  var randomChar = [];
+  for (var i = 0; i < userNumCharacters; i++) {
+    var index = Math.floor(Math.random() * chosenChar.length);
+    randomChar.push(chosenChar[index]);
+  }
+   
+  var replacedPosition = {};
+  while (guaranteedChar.length > 0) {
+    var replaceChar = Math.floor(Math.random() * randomChar.length);
+    if (!replacedPosition[replaceChar]) {
+      randomChar[replaceChar] = guaranteedChar.pop();
+      replacedPosition[replaceChar] = true;
+    }
+  }
+  return randomChar.join("");
+}
+
+// //* input pw */
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
+
 // //  generate btn
 generateBtn.addEventListener("click", writePassword);
 
